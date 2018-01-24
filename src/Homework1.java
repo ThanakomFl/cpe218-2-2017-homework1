58718d8ade23c0ec576b170d8138149a0bdb833c
ThanakomFl <30922226+ThanakomFl@users.noreply.github.com>
2018-01-24 20:49:46 +0700

    Add files via upload

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,22 +1,155 @@
-import javax.swing.JTree;
+
+import javax.swing.*;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import javax.swing.tree.TreeSelectionModel;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.util.Stack;
 
-public class Homework1 {
+public class Homework1 extends JPanel
+		implements TreeSelectionListener {
+
+	private JEditorPane htmlPane;
+	private JTree tree;
+
+	private static boolean playWithLineStyle = false;
+	private static String lineStyle = "Horizontal";
+
+	public static Node root;
+
+	public Homework1() {
+		super(new GridLayout(1,0));
+
+		DefaultMutableTreeNode top =
+				new DefaultMutableTreeNode(root);
+		createNodes(top,root);
+
+		tree = new JTree(top);
+		tree.getSelectionModel().setSelectionMode
+				(TreeSelectionModel.SINGLE_TREE_SELECTION);
+
+		tree.addTreeSelectionListener(this);
+
+		if (playWithLineStyle) {
+			System.out.println("line style = " + lineStyle);
+			tree.putClientProperty("JTree.lineStyle", lineStyle);
+		}
+
+		JScrollPane treeView = new JScrollPane(tree);
+
+		htmlPane = new JEditorPane();
+		htmlPane.setEditable(false);
+		JScrollPane htmlView = new JScrollPane(htmlPane);
+
+		JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+		splitPane.setTopComponent(treeView);
+		splitPane.setBottomComponent(htmlView);
+
+		Dimension minimumSize = new Dimension(100, 50);
+		htmlView.setMinimumSize(minimumSize);
+		treeView.setMinimumSize(minimumSize);
+		splitPane.setDividerLocation(100);
+		splitPane.setPreferredSize(new Dimension(500, 300));
+
+		ImageIcon leafIcon = createImageIcon("middle.gif");
+		if (leafIcon != null) {
+			DefaultTreeCellRenderer renderer =
+					new DefaultTreeCellRenderer();
+			renderer.setClosedIcon(leafIcon);
+			renderer.setOpenIcon(leafIcon);
+			tree.setCellRenderer(renderer);
+		}
+
+		add(splitPane);
+	}
+
+	/** Required by TreeSelectionListener interface. */
+	public void valueChanged(TreeSelectionEvent e) {
+		StringBuffer syntex ;
+		DefaultMutableTreeNode node = (DefaultMutableTreeNode)
+				tree.getLastSelectedPathComponent();
+		Object nodeInfo = node.getUserObject();
+		if (node == null) return;
+		if(node.isLeaf()){
+			htmlPane.setText(nodeInfo.toString());
+		}else{
+			syntex = getvalue((Node) nodeInfo);
+			htmlPane.setText(String.valueOf(syntex));
+		}
+	}
+
+
+	static public StringBuffer getvalue(Node n){
+		StringBuffer x = new StringBuffer(infix(n));
+		x.deleteCharAt(0);
+		x.deleteCharAt(x.length() - 1);
+		x.append("=");
+		x.append(calculate(n));
+	return x;
+	}
+
+	private void createNodes(DefaultMutableTreeNode top ,Node n) {
+		if(n.right!=null)
+		{
+			DefaultMutableTreeNode Right=new DefaultMutableTreeNode(n.right);
+			top.add(Right);
+			createNodes(Right,n.right);
+		}
+		if(n.left!=null)
+		{
+			DefaultMutableTreeNode left=new DefaultMutableTreeNode(n.left);
+			top.add(left);
+			createNodes(left,n.left);
+		}
+	}
+
+	public static ImageIcon createImageIcon(String path) {
+		java.net.URL imgURL = Homework1.class.getResource(path);
+		if (imgURL != null) {
+			return new ImageIcon(imgURL);
+		} else {
+			System.out.println("Couldn't find file!!!");
+			return null;
+		}
+	}
+
+	private static void createAndShowGUI() {
+
+		JFrame frame = new JFrame("Binary Tree Calculator");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		Homework1 newContentPane = new Homework1();
+		newContentPane.setOpaque(true); //content panes must be opaque
+		frame.setContentPane(newContentPane);
+
+		frame.pack();
+		frame.setVisible(true);
+	}
+
 	static public void main(String[] args) {
-		// Begin of arguments input sample
 		try {
-			Node current = new Node(args[0]);
-			current = Tostack(current);
-			StringBuffer x = new StringBuffer(infix(current));
+			root = new Node(args[0]);
+			root = Tostack(root);
+			StringBuffer x = new StringBuffer(infix(root));
 			x.deleteCharAt(0);
 			x.deleteCharAt(x.length() - 1);
 			System.out.print(x);
-			System.out.println("=" + calculate(current));
+			System.out.println("=" + calculate(root));
 			//System.out.print(inorder(current));
+		//	System.out.println(inorder(current));
+			javax.swing.SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					createAndShowGUI();
+				}
+			});
 		}catch (Exception e){
 			System.out.print("Invalid Input Try Again ..");
 		}
-		// End of arguments input sample
 	}
 
 	static public Node Tostack(Node n){
@@ -38,11 +171,9 @@ public class Homework1 {
 	}
 
 	static public String inorder(Node n){
-		if(n.left != null && n.right != null){
-			return (inorder(n.left) +" "+ n.ar + " " + inorder(n.right));
-		}else{
-			return (n.ar+"");
-		}
+		if( n.left != null && n.right != null){
+			return (n.ar +" " + inorder(n.right) + " " + inorder(n.left)) ;
+		}else return (n.ar+"");
 	}
 
 	static public int calculate(Node n){
@@ -111,6 +242,9 @@ public class Homework1 {
 		}
 		public Node(String k){
 			this.sr = k;
+		}
+		public String toString(){
+			return ar+"";
 		}
 	}
 
